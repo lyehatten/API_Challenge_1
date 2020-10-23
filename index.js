791b6df0719f423e27f1a3668129cf5713041dc4
lynqux <57960864+lynqux@users.noreply.github.com>
2020-10-23 15:04:27 -0400

    debug

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ fetch("https://ghibliapi.herokuapp.com/films").then(function(response){
     for(f of json){
         let filmBox = document.createElement("div");
         filmBox.classList.add("indivMovie");
-        let titleBox = document.createElement("div");
-        titleBox.classList.add("movieOne");
         let descriptionBox = document.createElement("div");
-        descriptionBox.classList.add("movieTwo")
-        titleBox.innerHTML = "<h3>" + f.title + "</h3>";
+        descriptionBox.classList.add("movieInfo")
+        filmBox.innerHTML = '<h3 id="a">' + f.title + '</h3>';
         descriptionBox.innerHTML = "<h4>Description:</h4> </br> <p>" + f.description + "</p>";
-        filmBox.appendChild(titleBox);
         filmBox.appendChild(descriptionBox);
         allMovies.appendChild(filmBox);
     }
